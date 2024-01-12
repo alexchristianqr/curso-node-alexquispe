9f4366325799fd2bdc7059aadfbbe7a37ecc6a0c
Alex Christian <alexchristianqr@gmail.com>
2024-01-12 15:16:41 -0500

    [updated]

--- pc02/problema-02/app-todolist/src/modules/todo/templates/TodoTemplate.vue
+++ pc02/problema-02/app-todolist/src/modules/todo/templates/TodoTemplate.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-import TodoForm from "../components/TodoForm.vue";
 import TodoList from "../components/TodoList.vue";
 </script>
 
 <template>
-  <!--  <TodoForm />-->
   <TodoList />
 </template>
 
