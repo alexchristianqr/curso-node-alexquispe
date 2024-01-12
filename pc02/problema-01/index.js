d6349490e3b0ca4cb2fd103931e0bd052288cc44
Alex Christian <alexchristianqr@gmail.com>
2024-01-11 21:33:45 -0500

    [added] mongoose

--- pc02/problema-01/index.js
+++ pc02/problema-01/index.js
@@ -1,6 +1,7 @@
 import { config } from "dotenv";
 import express from "express";
 import morgan from "morgan";
+import mongoose from "mongoose";
 import { todoRoute } from "./src/todo/todo.route.js";
 
 config();
@@ -14,8 +15,15 @@ app.get("/", (req, res) => {
   res.send("<h2>Welcome to API TODO</h2> <div>by <a href='https://github.com/alexchristianqr'>Alex Christian</a></div>.");
 });
 
-const port = process.env.PORT;
-
-app.listen(port, () => {
-  console.log(`Server on http://localhost:${port}`);
-});
+console.log("Connecting...");
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB >>> :)");
+    const port = process.env.PORT;
+    app.listen(port, () => console.log(`Server on http://localhost:${port}`));
+  })
+  .catch((error) => {
+    console.error(error);
+    console.log("Error not connected >>> :(");
+  });
