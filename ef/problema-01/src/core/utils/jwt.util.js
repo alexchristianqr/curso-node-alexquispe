2b70a6c32fbd7dd04770a6be1aa880434d37b55e
Alex Christian <alexchristianqr@gmail.com>
2024-01-14 23:44:34 -0500

    [updated] jwt util

---
+++ ef/problema-01/src/core/utils/jwt.util.js
@@ -0,0 +1,27 @@
+import JWT from "jsonwebtoken";
+
+export async function signJwtToken({ user = {}, timeExpiresAt = 0.05, oauthAccessToken = null }) {
+  // Jwt
+  let access_token, expires_at;
+  const secret = process.env.SECRET_API;
+
+  const timeToMiliseconds = timeExpiresAt * 60 * 60 * 1000;
+  expires_at = new Date(Date.now() + timeToMiliseconds); // (dias * horas) * minutos * segundos * milisegundos
+  const payload = {
+    data: { fullname: user.fullname, username: user.username },
+  };
+
+  // Validar login con algún proveedor
+  if (oauthAccessToken) {
+    access_token = oauthAccessToken;
+  } else {
+    access_token = JWT.sign(payload, secret, { expiresIn: expires_at.getTime() });
+  }
+
+  console.log({ access_token, expires_at });
+
+  return {
+    access_token,
+    expires_at,
+  };
+}
--- ef/problema-01/src/user/user.service.js
+++ ef/problema-01/src/user/user.service.js
@@ -1,4 +1,5 @@
 import { User } from "./user.schema.js";
+import { signJwtToken } from "../core/utils/jwt.util.js";
 
 class UserService {
   async getAll() {
@@ -8,7 +9,12 @@ class UserService {
     const { payload } = data;
     payload.created_at = new Date();
     payload.revoked = false;
-    payload.access_token = null;
+    const user = payload;
+
+    const { access_token, expires_at } = await signJwtToken({ user });
+    payload.access_token = access_token;
+    payload.expires_at = expires_at;
+
     return User.create(payload);
   }
 }
