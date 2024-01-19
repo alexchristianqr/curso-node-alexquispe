0de76af9acd3a11d149bd639135784d0a4a2f18d
Alex Christian <alexchristianqr@gmail.com>
2024-01-18 20:41:50 -0500

    [updated] Todo form

--- proyecto-final/frontend/app-todolist/src/modules/todo/components/TodoForm.vue
+++ proyecto-final/frontend/app-todolist/src/modules/todo/components/TodoForm.vue
@@ -73,13 +73,12 @@ const onReset = () => {
 
   <q-form class="q-gutter-md" @submit.prevent.stop="onSubmit(payloadForm)" @reset="onReset">
     <template v-if="actionForm.action === 'remove'">
-      <q-input :ref="uuidRef" outlined v-model="payloadForm._id" disable label="UUID" :rules="[(val) => !!val || 'Campo obligatorio']" />
+      <q-input :ref="descriptionRef" outlined v-model="payloadForm.description" disable label="Descripción" :rules="[(val) => !!val || 'Campo obligatorio']" />
     </template>
     <template v-else>
       <q-input :ref="descriptionRef" outlined v-model="payloadForm.description" label="Descripción" :rules="[(val) => !!val || 'Campo obligatorio']" />
       <q-select :ref="statusRef" outlined v-model="payloadForm.status" :options="statuses" map-options label="Estado" emit-value :rules="[(val) => !!val || 'Campo obligatorio']" />
     </template>
-
     <q-btn type="submit" color="primary" label="Guardar" v-if="actionForm.action === 'register'" :loading="loading" :disable="loading">
       <template v-slot:loading><q-spinner /></template>
     </q-btn>
